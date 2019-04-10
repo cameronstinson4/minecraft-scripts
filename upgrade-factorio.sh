3efc18d74f6506db0c9b35e9dcc6e24fecf3f151
Austin T Schaffer <schaffer.austin.t@gmail.com>
2019-04-09 21:10:50 -0400

    Created a Factorio server upgrade script.

---
+++ upgrade-factorio.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Upgrades a headless factorio installation using the lastest "experimental"
+# build. Assumes that factorio has been set up as a systemd service. Requires
+# `sudo` to run.
+
+DIST_URL="https://www.factorio.com/get-download/latest/headless/linux64"
+TAR_FILE="$HOME/factorio-latest.tar.xz"
+INSTALL_DIR="/factorio"
+
+echo "Downloading archive from: $DIST_URL"
+wget "$DIST_URL" -O "$TAR_FILE"
+
+echo "Stopping factorio service..."
+systemctl stop factorio.service
+
+echo "Extracting archive..."
+tar -xf "$TAR_FILE" -C "$INSTALL_DIR" --strip-components=1
+
+echo "Removing archive..."
+rm "$TAR_FILE"
+
+echo "Changing permissions..."
+chown -R factorio:factorio "$INSTALL_DIR"
+
+echo "Starting factorio service..."
+systemctl start factorio.service
+
+echo "Done."
