119f11716ce5042d182acb0e061c6b373dbcefa9
Cameron Stinson <cameronstinson4@gmail.com>
2019-06-24 23:34:57 -0400

    updated systemd config

--- install-minecraft.sh
+++ install-minecraft.sh
@@ -5,7 +5,7 @@
 # `sudo` to run.
 
 DIST_URL="https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar"
-TAR_FILE="$HOME/server.jar"
+TAR_FILE="/minecraft/server.jar"
 INSTALL_DIR="/minecraft"
 
 UPGRADE="$1"
